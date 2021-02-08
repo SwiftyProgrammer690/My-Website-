6957f3cde4d8fd4c21e3876a54eb93ba52d1d488
Swarit Choudhari <swaritchoudhari.p.s.y@gmail.com>
2021-02-08 17:31:46 +0000

    Initial commit

---
+++ phoneusers.js
@@ -0,0 +1,5 @@
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+  function myFunction() {
+    location.replace("https://SlightPolishedComments.swaritchoudhari.repl.co")
+  }
+}
